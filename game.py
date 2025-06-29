eadf8a60a230e558d28bb1c3a3b925b53d0a9293
peagod09 <ip9939002@gmail.com>
2025-06-29 10:26:11 +0300

    code

--- game.py
+++ game.py
@@ -4,9 +4,14 @@ game = True
 finish = False
 from random import *
 clock = time.Clock()
+speed_x = 3
+speed_y = 3
 mixer.init()
 font.init()
 background = transform.scale(image.load('задний фон.jpg'),(700,500))
+font1 = font.Font(None,35)
+lose1 = font1.render('PLAYER 1 LOSE', True,(180,0,0))
+lose2 = font1.render('PLAYER 2 LOSE', True,(180,0,0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_speed,player_x,player_y,scale_w,scale_h):
@@ -41,6 +46,18 @@ while game == True:
             game = False
     if finish == False:
         window.blit(background,(0,0))
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player1,ball) or sprite.collide_rect(player2,ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(200,200))
+        if ball.rect.x > 650:
+            finish = True
+            window.blit(lose2,(200,200))
         player1.update1()
         player1.reset()
         player2.update2()
